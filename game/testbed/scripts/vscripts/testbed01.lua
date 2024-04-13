5de5ade6b94e2fb73b04fd6839beb3adfb139bbb
QuackCola <23345567+QuackCola@users.noreply.github.com>
2024-04-13 17:07:57 -0400

    Create testbed01.lua

---
+++ game/testbed/scripts/vscripts/testbed01.lua
@@ -0,0 +1,25 @@
+local player = nil
+local target = nil
+
+function teleportPlayer()
+    -- get a handle to the player object
+    player = Entities:GetLocalPlayer()
+    -- Move The Player to a 'point_teleport' with the target name 'teleport_location'
+    teleportLocation = Entities:FindByName(nil,"teleport_location")
+    local pos = teleportLocation:GetOrigin()
+
+    player:SetOrigin(pos)
+end
+
+
+function printDebugStats()
+    -- get a handle to the player object
+    player = Entities:GetLocalPlayer()
+    -- Print Thy Stats
+    print("Printing out some player info:")
+    print(player:IsPlayer())
+    print(player:GetOrigin())
+    print(player:EyeAngles())
+    print(player:GetModelName())
+    print(player:EyePosition())
+end
